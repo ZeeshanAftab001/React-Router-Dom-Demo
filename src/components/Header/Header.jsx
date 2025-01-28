f5f61fa0ebc7414aa63a6a1158fe8469eba0dbca
zeeshan aftab <zeeshanaftababbasi@gmail.com>
2025-01-28 16:24:27 +0500

    Fourth commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import {Link, NavLink} from 'react-router-dom'
+import { Link, NavLink } from 'react-router-dom'
 
 export default function Header() {
     return (
         <header className="shadow sticky z-50 top-0">
-            <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+            { <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
                 <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
                     <Link to="/" className="flex items-center">
                         <img
@@ -34,8 +34,8 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
-                                to="/"
-                                    className={({isActive}) =>
+                                    to="/"
+                                    className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
@@ -44,8 +44,8 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
-                                to="/about"
-                                    className={({isActive}) =>
+                                    to="/about"
+                                    className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
@@ -54,8 +54,8 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
-                                to="/contact"
-                                    className={({isActive}) =>
+                                    to="/contact"
+                                    className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
@@ -64,11 +64,65 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
-                                to="/github"
-                                    className={({isActive}) =>
+                                    to="/github"
+                                    className={({ isActive }) =>
                                         `block py-2 pr-4 pl-3 duration-200 ${isActive ? "text-orange-700" : "text-gray-700"} border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
+                                    Github
+                                </NavLink>
+                            </li>
+
+                        </ul>
+                    </div>
+                </div>
+            </nav> }
+
+{/* 
+            <nav class="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 start-0 border-b border-gray-200 dark:border-gray-600">
+                <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+                    <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
+                        <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
+                            <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
+                    </a>
+                    <div class="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+                        <button data-collapse-toggle="navbar-sticky" type="button" class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-sticky" aria-expanded="false">
+                            <span class="sr-only">Open main menu</span>
+                            <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 1h15M1 7h15M1 13h15" />
+                            </svg>
+                        </button>
+                    </div>
+                    <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
+                        <ul class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+                            
+                            <li>
+                                <NavLink to="/" className={({isActive})=>{
+                                    `block py-2 px-3 ${isActive ? "text-orange-700" : "text-blue-700"} rounded-sm`
+                                }}>
+                                    Home
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/about" className={({isActive})=>{
+                                    `block py-2 px-3 ${isActive ? "text-orange-700" : "text-blue-700"} rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-70`
+                                }}>
+                                    About
+                                </NavLink>
+                            </li>
+                            <li>
+                                <NavLink to="/contact" className={({isActive})=>{
+                                    `block py-2 px-3 ${isActive ? "text-orange-700" : "text-blue-700"} rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-70`
+                                }}>
+                                    Contact
+                                </NavLink>
+                            </li>
+                            
+                            <li>
+                                <NavLink to="/github" className={({isActive})=>{
+                                    `block py-2 px-3 ${isActive ? "text-orange-700" : "text-blue-700"} rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-70`
+                                }}>
                                     Github
                                 </NavLink>
                             </li>
@@ -76,7 +130,8 @@ export default function Header() {
                         </ul>
                     </div>
                 </div>
-            </nav>
+            </nav> */}
+
         </header>
     );
 }
